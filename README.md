50c79bd82d2cbb11aa2bd5b964ed483532759a8a
theUniC <theunic@gmail.com>
2019-08-11 19:58:37 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ugly Trivia Kata
+
+Solution:
+
+**https://github.com/theUniC/ugly-trivia-kata/tree/solution**
---
+++ src/GameRunner.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UglyTriviaKata;
+
+include __DIR__.'/Game.php';
+
+$notAWinner;
+
+$aGame = new Game();
+
+$aGame->add("Chet");
+$aGame->add("Pat");
+$aGame->add("Sue");
+
+
+do {
+
+  $aGame->roll(rand(0,5) + 1);
+
+  if (rand(0,9) == 7) {
+      $notAWinner = $aGame->wrongAnswer();
+  } else {
+      $notAWinner = $aGame->wasCorrectlyAnswered();
+  }
+
+
+
+} while ($notAWinner);
